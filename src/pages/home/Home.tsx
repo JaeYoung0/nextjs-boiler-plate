17253b35a75c08aa4783d7668fa99b8c6f1fa9e1
JaeYoung0 <hjy8697@gmail.com>
2022-03-27 18:21:12 +0900

    Feat: emotion * emotion 관련 tsconfig, bablerc 설정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import * as S from './Home.style'
+import { css } from '@emotion/react'
 
 function Home() {
-  return <div>Home</div>
+  return (
+    <S.Container>
+      Home
+      <h1
+        css={css`
+          color: white;
+        `}
+      >
+        Home
+      </h1>
+    </S.Container>
+  )
 }
 
 export default Home
--- src/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-const IndexPage = () => <div>IndexPage</div>;
-
-export default IndexPage;
