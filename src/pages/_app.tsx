b7ebcbc016f37277f68f99ff7f752dd99e19e14e
허재영 <jaeyoung.heo@qualson.com>
2022-11-26 16:50:53 +0900

    Fix: add viewport meta tag for ios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Next.js Starter</title>
+        <meta name="viewport" content="width=device-width, viewport-fit=cover, initial-scale=1" />
       </Head>
       <Global styles={resetStyle} />
       <Component {...pageProps} />
