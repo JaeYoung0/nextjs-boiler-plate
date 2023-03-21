19bc06f29af15469ed2fe53e95d6954d095ccaff
JaeYoung0 <hjy8697@gmail.com>
2023-03-21 18:15:46 +0900

    Fix: svgr webpack

--- next.config.js
+++ next.config.js
@@ -1,10 +1,31 @@
+/** @type {import('next').NextConfig} */
+
 const path = require('path')
 const Dotenv = require('dotenv-webpack')
 
 const config = {
   webpack: (config) => {
+    // svgr
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
+
+    // Read the .env file
     config.plugins.push(
-      // Read the .env file
       new Dotenv({
         path: path.join(__dirname, '.env'),
         systemvars: true,
